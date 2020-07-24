9402cfe474fbd367c023594f83ae6125373cd1fa
Administrator <changpanzzp@163.com>
2020-07-24 11:42:07 +0800

    2020-07-24

---
+++ seata-order-service2002/src/main/java/com/alen/springcloud/service/impl/StorageServiceImpl.java
@@ -0,0 +1,31 @@
+package com.alen.springcloud.service.impl;
+
+import com.alen.springcloud.dao.StorageDao;
+import com.alen.springcloud.service.StorageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @Description: $
+ * @Param: $
+ * @return: $
+ * @Author: alen.ao
+ * @date: $
+ */
+@Service
+public class StorageServiceImpl implements StorageService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(StorageServiceImpl.class);
+
+    @Resource
+    private StorageDao storageDao;
+
+    @Override
+    public void decrease(Long productId, Integer count) {
+        LOGGER.info("----> storage-service中扣减库存开始");
+        storageDao.decrease(productId,count);
+        LOGGER.info("----> storage-service中扣减库存结束");
+    }
+}
